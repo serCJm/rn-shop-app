64192e3e404cde410521d2002f68b8d99bdb3f64
cjmoro <scjmoro@gmail.com>
2020-11-07 19:54:44 -0800

    Add initial screen

---
+++ screens/shop/ProductsOverviewScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FlatList, StyleSheet } from "react-native";
+
+interface Props {}
+
+const ProductsOverviewScreen: React.FC = (props: Props) => {
+	return <FlatList></FlatList>;
+};
+
+export default ProductsOverviewScreen;
+
+const styles = StyleSheet.create({});
