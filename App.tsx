078fec91ac8479802f8d55a6ba4b164930be27ba
cjmoro <scjmoro@gmail.com>
2020-11-07 19:55:44 -0800

    Format imports

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Provider } from "react-redux";
 import { combineReducers, createStore } from "redux";
+import { Provider } from "react-redux";
+
 import productsReducer from "./store/reducers/products";
 
 const rootReducer = combineReducers({
