8c598ddcab1ccb91919968c9fe1479f7c093967f
cjmoro <scjmoro@gmail.com>
2020-11-07 19:54:08 -0800

    Add dummy data

---
+++ models/product.ts
@@ -0,0 +1,17 @@
+export default class Product {
+	constructor(
+		public id: string,
+		public ownerId: string,
+		public title: string,
+		public imageUrl: string,
+		public description: string,
+		public price: number
+	) {
+		this.id = id;
+		this.ownerId = ownerId;
+		this.title = title;
+		this.imageUrl = imageUrl;
+		this.description = description;
+		this.price = price;
+	}
+}
