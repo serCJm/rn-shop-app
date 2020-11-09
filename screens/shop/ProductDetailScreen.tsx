81c0eaf609368de076de5ce6479e5207347074a4
cjmoro <scjmoro@gmail.com>
2020-11-08 19:41:24 -0800

    Add styling

--- screens/shop/ProductDetailScreen.tsx
+++ screens/shop/ProductDetailScreen.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+	Button,
+	Image,
+	ScrollView,
+	StyleSheet,
+	Text,
+	View,
+} from "react-native";
 import {
 	NavigationStackProp,
 	NavigationStackScreenComponent,
 } from "react-navigation-stack";
 import { useSelector } from "react-redux";
 import { RootState } from "../../App";
+import { Colors } from "../../constants/Colots";
 
 interface Props {
 	navigation: NavigationStackProp;
@@ -17,9 +25,25 @@ const ProductDetailScreen: NavigationStackScreenComponent = (props: Props) => {
 		state.products.availableProducts.find((prod) => prod.id === productId)
 	);
 	return (
-		<View>
-			<Text>{selectedProduct?.title}</Text>
-		</View>
+		<ScrollView>
+			<Image
+				style={styles.image}
+				source={{ uri: selectedProduct?.imageUrl }}
+			/>
+			<View style={styles.actions}>
+				<Button
+					color={Colors.PRIMARY}
+					title="Add to Cart"
+					onPress={() => {}}
+				></Button>
+			</View>
+			<Text style={styles.price}>
+				${selectedProduct?.price.toFixed(2)}
+			</Text>
+			<Text style={styles.description}>
+				{selectedProduct?.description}
+			</Text>
+		</ScrollView>
 	);
 };
 
@@ -31,4 +55,24 @@ ProductDetailScreen.navigationOptions = (navData) => {
 
 export default ProductDetailScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	image: {
+		width: "100%",
+		height: 300,
+	},
+	actions: {
+		marginVertical: 20,
+		alignItems: "center",
+	},
+	price: {
+		fontSize: 20,
+		color: "#888",
+		textAlign: "center",
+		marginVertical: 20,
+	},
+	description: {
+		fontSize: 14,
+		textAlign: "center",
+		marginHorizontal: 20,
+	},
+});
