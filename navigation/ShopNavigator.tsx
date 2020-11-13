1144b1d53a54c52f6921502d5fc4612041f529b9
cjmoro <scjmoro@gmail.com>
2020-11-12 19:38:33 -0800

    Add CartScreen

--- navigation/ShopNavigator.tsx
+++ navigation/ShopNavigator.tsx
@@ -5,11 +5,13 @@ import { Platform } from "react-native";
 import { Colors } from "../constants/Colots";
 import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
 import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
+import CartScreen from "../screens/shop/CartScreen";
 
 const ProductsNavigator = createStackNavigator(
 	{
 		ProductsOverview: ProductsOverviewScreen,
 		ProductDetail: ProductDetailScreen,
+		Cart: CartScreen,
 	},
 	{
 		defaultNavigationOptions: {
