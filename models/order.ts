45ca6f3ef91ad38c111cb017b18c0179d8653fe4
cjmoro <scjmoro@gmail.com>
2020-11-13 18:41:57 -0800

    Add getter for formated date

--- models/order.ts
+++ models/order.ts
@@ -1,3 +1,4 @@
+import moment from "moment";
 import CartItem from "./cart-item";
 
 export default class Order {
@@ -11,5 +12,16 @@ export default class Order {
 		this.items = items;
 		this.totalAmount = totalAmount;
 		this.date = date;
+	}
+
+	get readableDate() {
+		// return this.date.toLocaleDateString("en-EN", {
+		// 	year: "numeric",
+		// 	month: "long",
+		// 	day: "numeric",
+		// 	hour: "2-digit",
+		// 	minute: "2-digit",
+		// });
+		return moment(this.date).format("MMMM Do YYYY, hh:mm");
 	}
 }
