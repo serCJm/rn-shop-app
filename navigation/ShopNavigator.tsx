d7027c05501436ea6ff64390f6bd525c99435f2b
cjmoro <scjmoro@gmail.com>
2020-11-13 18:02:11 -0800

    Add drawer navigation to the navigator

--- navigation/ShopNavigator.tsx
+++ navigation/ShopNavigator.tsx
@@ -1,4 +1,5 @@
 import { createStackNavigator } from "react-navigation-stack";
+import { createDrawerNavigator } from "react-navigation-drawer";
 import { createAppContainer } from "react-navigation";
 import { Platform } from "react-native";
 
@@ -6,6 +7,20 @@ import { Colors } from "../constants/Colots";
 import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
 import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
 import CartScreen from "../screens/shop/CartScreen";
+import OrdersScreen from "../screens/shop/OrdersScreen";
+
+const defaultNavigationOptions = {
+	headerStyle: {
+		backgroundColor: Platform.OS === "android" ? Colors.PRIMARY : "",
+	},
+	headerTitleStyle: {
+		fontFamily: "open-sans-bold",
+	},
+	headerBackTitleStyle: {
+		fontFamily: "open-sans",
+	},
+	headerTintColor: Platform.OS === "android" ? "white" : Colors.PRIMARY,
+};
 
 const ProductsNavigator = createStackNavigator(
 	{
@@ -14,21 +29,27 @@ const ProductsNavigator = createStackNavigator(
 		Cart: CartScreen,
 	},
 	{
-		defaultNavigationOptions: {
-			headerStyle: {
-				backgroundColor:
-					Platform.OS === "android" ? Colors.PRIMARY : "",
-			},
-			headerTitleStyle: {
-				fontFamily: "open-sans-bold",
-			},
-			headerBackTitleStyle: {
-				fontFamily: "open-sans",
-			},
-			headerTintColor:
-				Platform.OS === "android" ? "white" : Colors.PRIMARY,
+		defaultNavigationOptions,
+	}
+);
+
+const OrdersNavigator = createStackNavigator(
+	{
+		Orders: OrdersScreen,
+	},
+	{ defaultNavigationOptions }
+);
+
+const ShopNavigator = createDrawerNavigator(
+	{
+		Products: ProductsNavigator,
+		Orders: OrdersNavigator,
+	},
+	{
+		contentOptions: {
+			activeTintColor: Colors.PRIMARY,
 		},
 	}
 );
 
-export default createAppContainer(ProductsNavigator);
+export default createAppContainer(ShopNavigator);
--- screens/shop/ProductsOverviewScreen.tsx
+++ screens/shop/ProductsOverviewScreen.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import {
+	NavigationStackOptions,
 	NavigationStackProp,
 	NavigationStackScreenComponent,
+	NavigationStackScreenProps,
 } from "react-navigation-stack";
 import { FlatList, Platform } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
@@ -11,12 +13,16 @@ import ProductItem from "../../components/shop/ProductItem";
 import * as cartActions from "../../store/actions/cart";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import CustomHeaderButton from "../../components/UI/CustomHeaderButton";
+import { NavigationDrawerScreenProps } from "react-navigation-drawer";
+import { NavigationScreenComponent } from "react-navigation";
 
 interface Props {
 	navigation: NavigationStackProp;
 }
+type Params = {};
+type ScreenProps = {};
 
-const ProductsOverviewScreen: NavigationStackScreenComponent = (
+const ProductsOverviewScreen: NavigationScreenComponent<Params, ScreenProps> = (
 	props: Props
 ) => {
 	const products = useSelector(
@@ -47,9 +53,22 @@ const ProductsOverviewScreen: NavigationStackScreenComponent = (
 	);
 };
 
-ProductsOverviewScreen.navigationOptions = (navData) => {
+type navOptions = NavigationStackScreenProps & NavigationDrawerScreenProps;
+
+ProductsOverviewScreen.navigationOptions = (navData: navOptions) => {
 	return {
 		headerTitle: "All Products",
+		headerLeft: () => (
+			<HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+				<Item
+					title="Menu"
+					iconName={
+						Platform.OS === "android" ? "md-menu" : "ios-cart"
+					}
+					onPress={() => navData.navigation.toggleDrawer()}
+				></Item>
+			</HeaderButtons>
+		),
 		headerRight: () => (
 			<HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
 				<Item
