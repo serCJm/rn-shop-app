af10b9762eb7fea4f2e9afbd67da7094bf5187e1
cjmoro <scjmoro@gmail.com>
2020-11-13 18:01:56 -0800

    Add Orders Screen

---
+++ screens/shop/OrdersScreen.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { FlatList, Platform, StyleSheet, Text } from "react-native";
+import { NavigationScreenComponent } from "react-navigation";
+import { NavigationDrawerScreenProps } from "react-navigation-drawer";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { NavigationStackScreenProps } from "react-navigation-stack";
+import { useSelector } from "react-redux";
+import { RootState } from "../../App";
+import CustomHeaderButton from "../../components/UI/CustomHeaderButton";
+
+interface Props {}
+type Params = {};
+type ScreenProps = {};
+
+const OrdersScreen: NavigationScreenComponent<Params, ScreenProps> = (
+	props: Props
+) => {
+	const orders = useSelector((state: RootState) => state.orders.orders);
+	return (
+		<FlatList
+			data={orders}
+			keyExtractor={(item) => item.id}
+			renderItem={(itemData) => <Text>{itemData.item.totalAmount}</Text>}
+		></FlatList>
+	);
+};
+
+type navOptions = NavigationStackScreenProps & NavigationDrawerScreenProps;
+
+OrdersScreen.navigationOptions = (navData: navOptions) => {
+	return {
+		headerTitle: "Your Orders",
+		headerLeft: () => (
+			<HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+				<Item
+					title="Menu"
+					iconName={
+						Platform.OS === "android" ? "md-menu" : "ios-cart"
+					}
+					onPress={() => navData.navigation.toggleDrawer()}
+				></Item>
+			</HeaderButtons>
+		),
+	};
+};
+
+export default OrdersScreen;
+
+const styles = StyleSheet.create({});
