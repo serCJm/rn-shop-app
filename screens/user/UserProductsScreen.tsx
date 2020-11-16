0744ef25082f6ce87e777096a4a5ef3c1b585779
cjmoro <scjmoro@gmail.com>
2020-11-15 21:50:41 -0800

    Add UserProducts screen

---
+++ screens/user/UserProductsScreen.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { FlatList, Platform, StyleSheet, Text, View } from "react-native";
+import { NavigationScreenComponent } from "react-navigation";
+import { NavigationDrawerScreenProps } from "react-navigation-drawer";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { NavigationStackScreenProps } from "react-navigation-stack";
+import { useSelector } from "react-redux";
+import { RootState } from "../../App";
+import ProductItem from "../../components/shop/ProductItem";
+import CustomHeaderButton from "../../components/UI/CustomHeaderButton";
+
+interface Props {}
+type Params = {};
+type ScreenProps = {};
+
+const UserProductsScreen: NavigationScreenComponent<Params, ScreenProps> = (
+	props: Props
+) => {
+	const userProducts = useSelector(
+		(state: RootState) => state.products.userProducts
+	);
+	return (
+		<FlatList
+			data={userProducts}
+			keyExtractor={(item) => item.id}
+			renderItem={(itemData) => (
+				<ProductItem
+					image={itemData.item.imageUrl}
+					title={itemData.item.title}
+					price={itemData.item.price}
+					onViewDetail={() => {}}
+					onAddToCard={() => {}}
+				></ProductItem>
+			)}
+		></FlatList>
+	);
+};
+
+type navOptions = NavigationStackScreenProps & NavigationDrawerScreenProps;
+
+UserProductsScreen.navigationOptions = (navData: navOptions) => {
+	return {
+		headerTitle: "Your Products",
+		headerLeft: () => (
+			<HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+				<Item
+					title="Menu"
+					iconName={
+						Platform.OS === "android" ? "md-menu" : "ios-cart"
+					}
+					onPress={() => navData.navigation.toggleDrawer()}
+				></Item>
+			</HeaderButtons>
+		),
+	};
+};
+
+export default UserProductsScreen;
+
+const styles = StyleSheet.create({});
