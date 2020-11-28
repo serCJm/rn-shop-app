f527c4055029bd7b9c67dc11c99e595c5a59f751
cjmoro <scjmoro@gmail.com>
2020-11-27 18:22:25 -0800

    Add state and dispatch login action

--- screens/user/AuthScreen.tsx
+++ screens/user/AuthScreen.tsx
@@ -1,5 +1,5 @@
 import { LinearGradient } from "expo-linear-gradient";
-import React from "react";
+import React, { useCallback, useReducer } from "react";
 import {
 	Button,
 	KeyboardAvoidingView,
@@ -7,13 +7,116 @@ import {
 	StyleSheet,
 	View,
 } from "react-native";
+import { NavigationScreenComponent } from "react-navigation";
+import { NavigationStackProp } from "react-navigation-stack";
+import { useDispatch } from "react-redux";
 import Card from "../../components/UI/Card";
 import Input from "../../components/UI/Input";
 import { Colors } from "../../constants/Colots";
+import * as authActions from "../../store/actions/auth";
 
-interface Props {}
+interface Props {
+	navigation: NavigationStackProp;
+}
+type Params = {};
+type ScreenProps = {};
 
-const AuthScreen = (props: Props) => {
+enum FormReducerActions {
+	REDUCER_UPDATE = "UPDATE",
+}
+
+interface IUpdateFormReducer {
+	type: typeof FormReducerActions.REDUCER_UPDATE;
+	value: string;
+	isValid: boolean;
+	input: string;
+}
+
+type FormReducerActionTypes = IUpdateFormReducer;
+
+interface IReducerState {
+	inputValues: {
+		email: string;
+		password: string;
+	};
+	inputValidities: {
+		email: boolean;
+		password: boolean;
+	};
+	formIsValid: boolean;
+}
+
+const formReducer = (
+	state: IReducerState,
+	action: FormReducerActionTypes
+): IReducerState => {
+	if (action.type === FormReducerActions.REDUCER_UPDATE) {
+		const updatedValues = {
+			...state.inputValues,
+			[action.input]: action.value,
+		};
+		const updatedValidities = {
+			...state.inputValidities,
+			[action.input]: action.isValid,
+		};
+		let formIsValid = true;
+		for (const val of Object.values(updatedValidities)) {
+			formIsValid = formIsValid && !!val;
+		}
+		return {
+			inputValues: updatedValues,
+			inputValidities: updatedValidities,
+			formIsValid,
+		};
+	}
+	return state;
+};
+
+const AuthScreen: NavigationScreenComponent<Params, ScreenProps> = (
+	props: Props
+) => {
+	const dispatch = useDispatch();
+	const [formState, dispatchFormState] = useReducer(formReducer, {
+		inputValues: {
+			email: "",
+			password: "",
+		},
+		inputValidities: {
+			email: false,
+			password: false,
+		},
+		formIsValid: false,
+	});
+
+	const signupHandler = () => {
+		dispatch(
+			authActions.signup(
+				formState.inputValues.email,
+				formState.inputValues.password
+			)
+		);
+	};
+
+	const inputChangeHandler = useCallback(
+		(
+			inputIdentifier: string,
+			inputValue: string,
+			inputValidity: boolean
+		) => {
+			// note, will run after leave focus
+			// so won't update state if trying to save
+			// before triggering finishing input
+			// will need to change
+			dispatchFormState({
+				type: FormReducerActions.REDUCER_UPDATE,
+				value: inputValue,
+				isValid: inputValidity,
+				input: inputIdentifier,
+			});
+		},
+		[dispatchFormState]
+	);
+
 	return (
 		<KeyboardAvoidingView
 			behavior="padding"
@@ -34,7 +137,7 @@ const AuthScreen = (props: Props) => {
 							email
 							autoCapitalize="none"
 							errorText="Please enter a valid email address!"
-							onInputChange={() => {}}
+							onInputChange={inputChangeHandler}
 							initialValue=""
 							initiallyValid={true}
 						></Input>
@@ -47,7 +150,7 @@ const AuthScreen = (props: Props) => {
 							required
 							autoCapitalize="none"
 							errorText="Please enter a valid password!"
-							onInputChange={() => {}}
+							onInputChange={inputChangeHandler}
 							initialValue=""
 							initiallyValid={true}
 						></Input>
@@ -62,7 +165,7 @@ const AuthScreen = (props: Props) => {
 							<Button
 								title="Switch To Sign Up"
 								color={Colors.ACCENT}
-								onPress={() => {}}
+								onPress={signupHandler}
 							></Button>
 						</View>
 					</ScrollView>
