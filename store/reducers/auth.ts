45f7dca9edc3c8a47e5d517511e887ae8b049dfb
cjmoro <scjmoro@gmail.com>
2020-12-04 21:39:02 -0800

    Add auth reducer

---
+++ store/reducers/auth.ts
@@ -0,0 +1,24 @@
+import { AuthState, LOGIN, SIGNUP } from "../types";
+import { AuthActionTypes } from "../types";
+
+const initialState: AuthState = {
+	token: "",
+	userId: "",
+};
+
+export default (state = initialState, action: AuthActionTypes) => {
+	switch (action.type) {
+		case LOGIN:
+			return {
+				token: action.token,
+				userId: action.userId,
+			};
+		case SIGNUP:
+			return {
+				token: action.token,
+				userId: action.userId,
+			};
+		default:
+			return state;
+	}
+};
