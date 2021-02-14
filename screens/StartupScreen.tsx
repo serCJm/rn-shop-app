6c2f1780268cd34dbe92672d2df3c1db85a4f3af
cjmoro <scjmoro@gmail.com>
2021-02-13 18:26:50 -0800

    Remove unused  + error handling

--- screens/StartupScreen.tsx
+++ screens/StartupScreen.tsx
@@ -3,7 +3,6 @@ import {
 	ActivityIndicator,
 	AsyncStorage,
 	StyleSheet,
-	Text,
 	View,
 } from "react-native";
 import { useDispatch } from "react-redux";
@@ -17,32 +16,36 @@ const StartupScreen = (props: Props) => {
 	const dispatch = useDispatch();
 	useEffect(() => {
 		const tryLogin = async () => {
-			const userData = await AsyncStorage.getItem("userData");
-			if (!userData) {
-				// props.navigation.navigate("Auth");
-				dispatch(authActions.setDidTryAL());
-				return;
-			}
-			const transformedData = JSON.parse(userData);
-			const { token, userId, expiryDate } = transformedData;
-			const expirationDate = new Date(expiryDate);
-			if (expirationDate <= new Date() || !token || !userId) {
-				// props.navigation.navigate("Auth");
-				dispatch(authActions.setDidTryAL());
-				return;
-			}
-			const expirationTime =
-				expirationDate.getTime() - new Date().getTime();
-			// props.navigation.navigate("Shop");
+			try {
+				const userData = await AsyncStorage.getItem("userData");
+				if (!userData) {
+					// props.navigation.navigate("Auth");
+					dispatch(authActions.setDidTryAL());
+					return;
+				}
+				const transformedData = JSON.parse(userData);
+				const { token, userId, expiryDate } = transformedData;
+				const expirationDate = new Date(expiryDate);
+				if (expirationDate <= new Date() || !token || !userId) {
+					// props.navigation.navigate("Auth");
+					dispatch(authActions.setDidTryAL());
+					return;
+				}
+				const expirationTime =
+					expirationDate.getTime() - new Date().getTime();
+				// props.navigation.navigate("Shop");
 
-			dispatch(
-				authActions.authenticate(
-					AUTHENTICATE,
-					userId,
-					token,
-					expirationTime
-				)
-			);
+				dispatch(
+					authActions.authenticate(
+						AUTHENTICATE,
+						userId,
+						token,
+						expirationTime
+					)
+				);
+			} catch (e) {
+				console.log(e);
+			}
 		};
 		tryLogin();
 	}, [dispatch]);
