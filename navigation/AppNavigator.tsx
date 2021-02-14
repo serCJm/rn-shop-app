596a7144ef87e8e203e5a2c132d99f93bfac66b6
cjmoro <scjmoro@gmail.com>
2021-02-13 18:26:02 -0800

    Fix activity indicator

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -10,14 +10,14 @@ interface Props {}
 const AppNavigator = (props: Props) => {
 	const isAuth = useSelector((state: RootState) => !!state.auth.token);
 	const didTryAutoLogin = useSelector(
-		(state: RootState) => !!state.auth.didTryAutoLogin
+		(state: RootState) => state.auth.didTryAutoLogin
 	);
 
 	return (
 		<NavigationContainer>
 			{isAuth && <ShopNavigator></ShopNavigator>}
 			{!isAuth && didTryAutoLogin && <AuthNavigator></AuthNavigator>}
-			<StartupScreen></StartupScreen>
+			{!isAuth && !didTryAutoLogin && <StartupScreen></StartupScreen>}
 		</NavigationContainer>
 	);
 };
